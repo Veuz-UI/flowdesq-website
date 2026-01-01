e9aa43743d6e5ba618a8e855a5073c5426cf74e4
shereef <shareef@veuz.in>
2026-01-01 12:30:53 +0530

    pricing changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -239,19 +239,21 @@ tabButtons.forEach(button => {
 
 
 // return scroll
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#scroll').fadeIn(); 
-        } else { 
-            $('#scroll').fadeOut(); 
-        } 
-    }); 
-    $('#scroll').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#scroll').fadeIn();
+    } else {
+      $('#scroll').fadeOut();
+    }
+  });
+
+  $('#scroll').click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 600);
+  });
 });
+
 
 
 
