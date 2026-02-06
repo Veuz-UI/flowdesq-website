f97fb25016d97f6121c2cd677cd111e74fa59616
shereef <shareef@veuz.in>
2026-02-06 12:05:34 +0530

    cancel smooth scroll

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,20 +1,22 @@
 
-//Animations
-gsap.registerPlugin(ScrollTrigger);
+// //Animations
+// gsap.registerPlugin(ScrollTrigger);
 
-//scroller-smooth
-const lenis = new Lenis()
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+// //scroller-smooth
+// const lenis = new Lenis()
+// lenis.on('scroll', (e) => {
+//   console.log(e)
+// })
 
-lenis.on('scroll', ScrollTrigger.update)
+// lenis.on('scroll', ScrollTrigger.update)
 
-gsap.ticker.add((time)=>{
-  lenis.raf(time * 300)
-})
-gsap.ticker.lagSmoothing(0)
-// close
+// gsap.ticker.add((time)=>{
+//   lenis.raf(time * 300)
+// })
+// gsap.ticker.lagSmoothing(0)
+// // close
+
+
 
 
 
